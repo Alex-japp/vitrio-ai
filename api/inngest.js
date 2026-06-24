07aa57343d4acd870a71605f8a01620921d58ff6
Alex-japp <starnelsemijoias@gmail.com>
2026-06-24 15:07:45 -0300

    Update inngest.js

--- api/inngest.js
+++ api/inngest.js
@@ -1,5 +1,5 @@
 const { Inngest } = require('inngest');
-const { serve } = require('inngest/node');
+const { serve } = require('inngest/express');
 const { getServiceAccountToken } = require('./auth-helper');
 const sharp = require('sharp');
 
